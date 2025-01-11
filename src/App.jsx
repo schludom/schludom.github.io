e6ed36f8ba94630b26907444fc5ec42af1e3f351
dominik.schlueter <dominik.schlueter@siemens-energy.com>
2025-01-11 18:19:47 +0100

    hashed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Routes, Route, HashRouter  } from "react-router-dom"
+import Subpage from './pages/Subpage'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,28 +9,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} of 100
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HashRouter>
+    <Routes>
+      <Route exact path="/home" element={<Subpage />} />
+    </Routes>
+    </HashRouter>
   )
 }
 
---
+++ src/pages/Subpage.jsx
@@ -0,0 +1,12 @@
+
+
+const Subpage = () => {
+
+  return (
+      <div>
+        hello
+    </div>
+  )
+}
+
+export default Subpage
